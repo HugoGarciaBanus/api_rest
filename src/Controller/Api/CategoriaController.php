1d34cfdcb42d98e7db674a0986397b36e40d1ab6
HugoGarciaBanus <sergio.garcia@solvam.es>
2022-10-11 19:36:44 +0200

    martes 04

--- src/Controller/Api/CategoriaController.php
+++ src/Controller/Api/CategoriaController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Categoria;
+use App\Form\CategoriaType;
 use App\Repository\CategoriaRepository;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Rest\Route("/categoria")
@@ -12,6 +17,8 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 
 class CategoriaController extends AbstractFOSRestController
 {
+    //CRUD
+    //create, update, read, delete
 
     private $categoriaRepository;
 
@@ -20,6 +27,7 @@ class CategoriaController extends AbstractFOSRestController
         $this->categoriaRepository = $repo;
     }
 
+    // Traer todas las categorias
     /**
      * @Rest\Get(path="/")
      * @Rest\View (serializerGroups={"get_categorias"}, serializerEnableMaxDepthChecks = true)
@@ -27,5 +35,108 @@ class CategoriaController extends AbstractFOSRestController
 
     public function getCategorias(){
         return $this->categoriaRepository->findAll();
+    }
+
+
+
+    // Traer un categoria
+    /**
+     * @Rest\Get (path="/{id}")
+     * @Rest\View(serializerGroups={"get_categoria"}, serializerEnableMaxDepthChecks = true)
+     */
+
+    public function getCategoria(Request $request){
+        $idCategoria = $request->get('id');
+        $categoria= $this->categoriaRepository->find($idCategoria);
+        if(!$categoria){
+            return new JsonResponse('No se ha encontrado la categoria', Response::HTTP_NOT_FOUND);
+        }
+        return $categoria;
+    }
+
+//    /**
+//     * @Rest\Post (path="/")
+//     * @Rest\View (serializerGroups={"post_categoria"}, serializerEnableMaxDepthChecks = true)
+//     */
+//    public function createCategoria(Request $request){
+//        $categoria = $request->get('categoria');
+//
+//        if(!$categoria){ //Comprueba si llega un dato pero no el tipo
+//            return new JsonResponse('error en la petición', Response::HTTP_BAD_REQUEST);
+//        }
+//        // Crear el objeto y hacer un set del nombre de la categoria recibida
+//        $cat = new Categoria();
+//        $cat->setCategoria($categoria);
+//        // Guardamos en base de datos
+//        $this->categoriaRepository->add($cat, true);
+//        // Enviar una respuesta al cliente
+//        return $cat;
+//
+//    }
+
+
+    /**
+     * @Rest\Post (path="/")
+     * @Rest\View (serializerGroups={"post_categoria"}, serializerEnableMaxDepthChecks = true)
+     */
+
+    public function createCategoria(Request $request){
+        // Formularios sirven para manejar las peticiones y validar tipado (Null, si viene 1 string vacio...
+        //Asociar formulario con validator -> bundle de symfony
+        // 1. Creo el objeto vacio
+        $cat = new Categoria();
+        // 2. Inicalizar el form
+        $form = $this->createForm(CategoriaType::class, $cat);
+        //3. Le decimos al formulario que maneje la request
+        $form->handleRequest($request);
+        //4. Comprobar si hay error
+        if(!$form->isSubmitted() || !$form->isValid()){
+            return $form;
+        }
+        //5. Correcto-> guardo en BD
+        $categoria = $form->getData();
+        $this->categoriaRepository->add($categoria, true);
+        return $categoria;
+    }
+
+
+    // UPDATE CON PATCH (parche para updatear una categoria y no todas (put))
+
+    /**
+     * @Rest\Patch (path="/{id}")
+     * @Rest\View (serializerGroups={"up_categoria"}, serializerEnableMaxDepthChecks = true)
+     */
+    public function updateCategoria(Request $request){
+        //Me guardo el id de la categoria
+        $categoriaId = $request->get('id');
+        //OJO comprobar que existe esa categoria
+        $categoria = $this->categoriaRepository->find($categoriaId);
+        if(!$categoria){
+            return new JsonResponse('No se ha encontrado', Response::HTTP_NOT_FOUND);
+        }
+        $form = $this->createForm(CategoriaType::class, $categoria, ['method'=>$request->getMethod()]);
+        $form->handleRequest(($request));
+        // Tenemos que comprobar la validez del form
+        if(!$form->isSubmitted() || !$form->isValid()){
+            return new JsonResponse('Bad data', 404);
+        }
+        $this->categoriaRepository->add($categoria, true);
+        return $categoria;
+    }
+
+    // DELETE
+    /**
+     * @Rest\Delete (path="/{id}")
+     *
+     */
+
+    public function deleteCategoria(Request $request){
+        $idCategoria = $request->get('id');
+        $categoria = $this->categoriaRepository->find($idCategoria);
+        if(!$categoria){
+            return new JsonResponse('No se ha encontrado la categoria', Response::HTTP_NOT_FOUND);
+        }
+        $this->categoriaRepository->remove($categoria, true);
+        return new JsonResponse('Categoria Borrada', 200);
     }
 }
---
+++ src/Controller/Api/DireccionController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\Direccion;
+use App\Form\DireccionType;
+use App\Repository\ClienteRepository;
+use App\Repository\DireccionRepository;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Test\Constraint\RequestAttributeValueSame;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ * @Rest\Route("/direccion")
+ */
+class DireccionController extends AbstractFOSRestController
+{
+    private $repo;
+
+    public function __construct(DireccionRepository $repo){
+        $this->repo = $repo;
+    }
+
+    /**
+     * @Rest\Post("/")
+     * @Rest\View (serializerGroups={"post_dir"}, serializerEnableMaxDepthChecks = true)
+     */
+
+    public function createDireccion(Request $request){
+        $direccion = new Direccion();
+        $form = $this->createForm(DireccionType::class, $direccion);
+        $form->handleRequest($request);
+        if(!$form->isSubmitted() || !$form->isValid()){
+            return new JsonResponse('Bad data', Response::HTTP_BAD_REQUEST);
+        }
+        $this->repo->add($direccion, true);
+        return $direccion;
+    }
+
+    // end point que devuelva todas las direcciones en base al id de un usuario
+    /**
+     * @Rest\Get  (path="/{id}")
+     * @Rest\View (serializerGroups={"get_dir_cliente"}, serializerEnableMaxDepthChecks= true)
+     */
+
+    public function getDirecccionesByCliente(Request $request, ClienteRepository  $clienteRepository){
+        $idCliente = $request-> get('id');
+        // 1. Traer el cliente de BD
+        $cliente = $clienteRepository->find($idCliente);
+        // 2. Una vez tenemos el cliente, si no existe devuelvo error
+        if(!$cliente){
+            return new JsonResponse('No se encontró el cliente', Response::HTTP_NOT_FOUND);
+        }
+        // 3. Si sí existe busco en l atabla direccion por el campo cliente
+        $direcciones = $this->repo->findBy(['cliente' => $cliente]);
+        return $direcciones;
+    }
+
+    // UPDATE
+    /**
+     * @Rest\Patch (path=/{id}")
+     * @Rest\View (serializerGroups={"post_dir"}, serializerEnableMaxDepthChecks=true)
+     */
+
+    public function updateDireccion(Request $request){
+        $idDireccion = $request->get('id');
+        $direccion = $this->repo->find($idDireccion);
+        if(!$direccion){
+            return new JsonResponse('No existe', 404);
+        }
+        $form = $this->createForm(DireccionType::class, $direccion, ['method'=> $request->getMethod()]);
+        $form ->handleRequest($request);
+        if(!$form->isSubmitted() || !$form->isValid()){
+            return $form;
+        }
+
+        $this->repo-> add($direccion, true);
+        return $direccion;
+    }
+
+    // DELETE
+    /**
+     * @Rest\Delete (path=/{"id}")
+     */
+
+    public function deleteDireccion(Request $request){
+        $idDireccion = $request->get('id');
+        $direccion = $this->repo->find($idDireccion);
+        if(!$direccion){
+            throw new NotFoundHttpException('No existe la dirección');
+        }
+        $this->repo->remove($direccion, true);
+        return new JsonResponse('Eliminado', Response::HTTP_OK);
+    }
+}
